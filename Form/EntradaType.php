0aab8b415a27342bec1f55aab48f94e95d1f7145
mshtefec <max.shtefec@gmail.com>
2016-05-31 12:53:34 -0300

    cambios foro 31-05-16

--- Form/EntradaType.php
+++ Form/EntradaType.php
@@ -12,14 +12,15 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
  */
 class EntradaType extends AbstractType
 {
-
+    /*
     private $idForo;
 
     public function __construct($idForo)
     {
         $this->idForo = $idForo;
     }
-
+    */
+    
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
@@ -28,10 +29,10 @@ class EntradaType extends AbstractType
     {
         $builder
             ->add('titulo')
-            ->add('idForo', 'hidden', array(
-                'mapped' => false,
-                'data'   => $this->idForo,
-            ))
+            // ->add('idForo', 'hidden', array(
+            //     'mapped' => false,
+            //     'data'   => $this->idForo,
+            // ))
             // ->add('autor', 'select2', array(
             //     'class' => 'MWSimple\Bundle\ForoBundle\Entity\Usuario',
             //     'url'   => 'Entrada_autocomplete_autor',
